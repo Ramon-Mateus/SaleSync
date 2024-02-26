9c7804f331e9abd2371e8fc2f018be44aa03a510
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-02-26 17:52:42 -0300

    Add: Melhoria na documentação da API no Swagger

--- src/SaleSync.API/Controllers/AuctionController.cs
+++ src/SaleSync.API/Controllers/AuctionController.cs
@@ -1,5 +1,6 @@
 ﻿using SaleSync.API.UseCases.Auctions.GetCurrent;
 using Microsoft.AspNetCore.Mvc;
+using SaleSync.API.Entities;
 
 namespace SaleSync.API.Controllers
 {
@@ -8,11 +9,18 @@ namespace SaleSync.API.Controllers
     public class AuctionController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)] // Melhora a documentação do Swagger
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // Melhora a documentação do Swagger
         public IActionResult GetCurrentAuction()
         {
             GetCurrentAuctionUseCase useCase = new GetCurrentAuctionUseCase(); // Também funciona assim: var useCase = new GetCurrentAuctionUseCase();
 
             var result = useCase.Execute();
+
+            if (result != null)
+            {
+                return NoContent();
+            }
 
             return Ok(result);
         }
