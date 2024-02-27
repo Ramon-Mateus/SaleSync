f1f2b88e9b29df183c56f401fa8f478acf148b26
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-02-27 04:54:54 -0300

    Add: Enum para dar sentido aos numeros das conditions dos items

--- src/SaleSync.API/Entities/Item.cs
+++ src/SaleSync.API/Entities/Item.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using SaleSync.API.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SaleSync.API.Entities
 {
@@ -8,7 +9,7 @@ namespace SaleSync.API.Entities
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
         public string Brand { get; set; } = String.Empty;
-        public int Condition { get; set; }
+        public Condition Condition { get; set; }
         public decimal BasePrice { get; set; }
         public int AuctionId { get; set; }
     }
